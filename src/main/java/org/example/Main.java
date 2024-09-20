27f08e5720ef91d4d3a1c30b0b820ae8cf8bdb5e
Thomas Gebert <thomas@gebert.app>
2024-09-19 23:24:44 -0400

    Run the formatter.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,46 +1,49 @@
 package org.example;
 
+import java.util.concurrent.LinkedBlockingQueue;
 import tombert.csp.CSP;
 
-import java.util.concurrent.LinkedBlockingQueue;
-
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
+  public static void main(String[] args) {
 
-        var init = CSP.returned("howdy");
+    var init = CSP.returned("howdy");
 
-        var a = CSP.map(x -> {
-            return x + " tom";
-        }, init);
+    var a =
+        CSP.map(
+            x -> {
+              return x + " tom";
+            },
+            init);
 
-        var b = CSP.bind(x -> {
-            var rc = new LinkedBlockingQueue<String>();
-            Thread.startVirtualThread(() -> {
-                while (true) {
-                    try {
+    var b =
+        CSP.bind(
+            x -> {
+              var rc = new LinkedBlockingQueue<String>();
+              Thread.startVirtualThread(
+                  () -> {
+                    while (true) {
+                      try {
                         Thread.sleep(1000);
                         var z = x + " poop";
                         rc.put(z);
-                    } catch (InterruptedException e) {
+                      } catch (InterruptedException e) {
                         throw new RuntimeException(e);
+                      }
                     }
-                }
-            });
-            return rc;
-        }, a);
+                  });
+              return rc;
+            },
+            a);
 
-        while (true) {
-            try {
-                var res = b.take();
-                System.out.println(res);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-        }
-
-
-
+    while (true) {
+      try {
+        var res = b.take();
+        System.out.println(res);
+      } catch (InterruptedException e) {
+        throw new RuntimeException(e);
+      }
     }
+  }
 }
--- src/main/java/tombert/csp/CSP.java
+++ src/main/java/tombert/csp/CSP.java
@@ -3,7 +3,6 @@ package tombert.csp;
 import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
-
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.LinkedBlockingQueue;
@@ -11,192 +10,202 @@ import java.util.function.Function;
 import java.util.function.Supplier;
 
 public class CSP {
-    public enum Signals {
-        DONE;
-    }
+  public enum Signals {
+    DONE;
+  }
 
-    public static <R> BlockingQueue<R> returned(R init) {
-        var retChan = new LinkedBlockingQueue<R>();
-        retChan.offer(init);
-        return retChan;
-    }
+  public static <R> BlockingQueue<R> returned(R init) {
+    var retChan = new LinkedBlockingQueue<R>();
+    retChan.offer(init);
+    return retChan;
+  }
 
-    public static <R> BlockingQueue<List<R>> chunk(int size, BlockingQueue<R> init) {
-        var retChan = new LinkedBlockingQueue<List<R>>();
+  public static <R> BlockingQueue<List<R>> chunk(int size, BlockingQueue<R> init) {
+    var retChan = new LinkedBlockingQueue<List<R>>();
 
-        Thread.startVirtualThread(() -> {
-            var buffer = new ArrayList<R>();
-            while (true) {
-                try {
-                    var current = init.take();
-                    buffer.add(current);
-                    if (buffer.size() >= size) {
-                        retChan.put(buffer);
-                    }
-                    buffer = new ArrayList<>();
-                } catch (InterruptedException e) {
-                    Thread.currentThread().interrupt();
-                    break;
-                }
-            }
-
-        });
-        return retChan;
-    }
-
-    public static <V,R> BlockingQueue<R> bind(Function<V, BlockingQueue<R>> f, BlockingQueue<V> init) {
-        var retChan = new LinkedBlockingQueue<R>();
-        Thread.startVirtualThread(() -> {
-            while (true) {
-                try {
-                    var current = init.take();
-                    var next =  f.apply(current);
-                    Thread.startVirtualThread(() -> {
-                        while (true) {
-                            try {
-                                var next2 = next.take();
-                                retChan.put(next2);
-                            } catch (InterruptedException e) {
-                                Thread.currentThread().interrupt();
-                                break;
-                            }
-                        }
-                    });
-                } catch (InterruptedException e) {
-                    Thread.currentThread().interrupt();
-                    break;
-                }
-            }
-        });
-        return retChan;
-    }
-
-    public static <V, R> BlockingQueue<R> map(Function<V, R> f, BlockingQueue<V> init)  {
-        var retChan = new LinkedBlockingQueue<R>();
-        Thread.startVirtualThread( () -> {
-            while (true) {
-                try {
-                    var current = init.take();
-                    var newThing = f.apply(current);
-                    retChan.put(newThing);
-                } catch (InterruptedException e) {
-                    Thread.currentThread().interrupt();
-                    break;
-                }
-            }
-        });
-        return retChan;
-    }
-
-    public static <R> BlockingQueue<R> filter(Function<R, Boolean> f, BlockingQueue<R> init) {
-        var retChan = new LinkedBlockingQueue<R>();
-        Thread.startVirtualThread(() -> {
-           while (true) {
-               try {
-                   var current = init.take();
-                   var res = f.apply(current);
-                   if (res) {
-                       retChan.put(current);
-                   }
-               } catch (InterruptedException e) {
-                   Thread.currentThread().interrupt();
-                   break;
-               }
-
-           }
-        });
-        return retChan;
-    }
-
-    public static BlockingQueue<Signals> timeout(Duration tout) {
-        var retChan = new LinkedBlockingQueue<Signals>(1);
-        Thread.startVirtualThread(() -> {
+    Thread.startVirtualThread(
+        () -> {
+          var buffer = new ArrayList<R>();
+          while (true) {
             try {
-                Thread.sleep(tout.toMillis());
-                retChan.put(Signals.DONE);
+              var current = init.take();
+              buffer.add(current);
+              if (buffer.size() >= size) {
+                retChan.put(buffer);
+              }
+              buffer = new ArrayList<>();
             } catch (InterruptedException e) {
-                Thread.currentThread().interrupt();
+              Thread.currentThread().interrupt();
+              break;
             }
+          }
         });
-        return retChan;
-    }
-    public static <V> BlockingQueue<V> go(Supplier<V> f ){
-       var retChan = new LinkedBlockingQueue<V>(1);
-       Thread.startVirtualThread(() -> {
-           var res = f.get();
-           try {
-               retChan.put(res);
-           } catch (InterruptedException e) {
-               Thread.currentThread().interrupt();
-           }
+    return retChan;
+  }
 
-       });
-       return retChan;
-    }
+  public static <V, R> BlockingQueue<R> bind(
+      Function<V, BlockingQueue<R>> f, BlockingQueue<V> init) {
+    var retChan = new LinkedBlockingQueue<R>();
+    Thread.startVirtualThread(
+        () -> {
+          while (true) {
+            try {
+              var current = init.take();
+              var next = f.apply(current);
+              Thread.startVirtualThread(
+                  () -> {
+                    while (true) {
+                      try {
+                        var next2 = next.take();
+                        retChan.put(next2);
+                      } catch (InterruptedException e) {
+                        Thread.currentThread().interrupt();
+                        break;
+                      }
+                    }
+                  });
+            } catch (InterruptedException e) {
+              Thread.currentThread().interrupt();
+              break;
+            }
+          }
+        });
+    return retChan;
+  }
 
-    public static BlockingQueue<Signals> go(Runnable f) {
-        var retChan = new LinkedBlockingQueue<Signals>();
-        Thread.startVirtualThread(() -> {
+  public static <V, R> BlockingQueue<R> map(Function<V, R> f, BlockingQueue<V> init) {
+    var retChan = new LinkedBlockingQueue<R>();
+    Thread.startVirtualThread(
+        () -> {
+          while (true) {
+            try {
+              var current = init.take();
+              var newThing = f.apply(current);
+              retChan.put(newThing);
+            } catch (InterruptedException e) {
+              Thread.currentThread().interrupt();
+              break;
+            }
+          }
+        });
+    return retChan;
+  }
+
+  public static <R> BlockingQueue<R> filter(Function<R, Boolean> f, BlockingQueue<R> init) {
+    var retChan = new LinkedBlockingQueue<R>();
+    Thread.startVirtualThread(
+        () -> {
+          while (true) {
+            try {
+              var current = init.take();
+              var res = f.apply(current);
+              if (res) {
+                retChan.put(current);
+              }
+            } catch (InterruptedException e) {
+              Thread.currentThread().interrupt();
+              break;
+            }
+          }
+        });
+    return retChan;
+  }
+
+  public static BlockingQueue<Signals> timeout(Duration tout) {
+    var retChan = new LinkedBlockingQueue<Signals>(1);
+    Thread.startVirtualThread(
+        () -> {
+          try {
+            Thread.sleep(tout.toMillis());
+            retChan.put(Signals.DONE);
+          } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+          }
+        });
+    return retChan;
+  }
+
+  public static <V> BlockingQueue<V> go(Supplier<V> f) {
+    var retChan = new LinkedBlockingQueue<V>(1);
+    Thread.startVirtualThread(
+        () -> {
+          var res = f.get();
+          try {
+            retChan.put(res);
+          } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+          }
+        });
+    return retChan;
+  }
+
+  public static BlockingQueue<Signals> go(Runnable f) {
+    var retChan = new LinkedBlockingQueue<Signals>();
+    Thread.startVirtualThread(
+        () -> {
+          f.run();
+          try {
+            retChan.put(Signals.DONE);
+          } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+          }
+        });
+    return retChan;
+  }
+
+  public static void goLoop(Runnable f) {
+    Thread.startVirtualThread(
+        () -> {
+          while (true) {
             f.run();
-            try {
-                retChan.put(Signals.DONE);
-            } catch (InterruptedException e) {
+          }
+        });
+  }
+
+  public static BlockingQueue<Object> select(List<BlockingQueue<Object>> channels) {
+    var chans = channels.toArray(BlockingQueue[]::new);
+    return select(chans);
+  }
+
+  @SafeVarargs
+  public static BlockingQueue<Object> select(BlockingQueue<Object>... channels) {
+    var results = new LinkedBlockingQueue<CompletableFuture<Object>>(channels.length);
+    for (var chan : channels) {
+      var f =
+          CompletableFuture.supplyAsync(
+              () -> {
+                try {
+                  return chan.take();
+                } catch (InterruptedException e) {
+                  throw new RuntimeException(e);
+                }
+              });
+      results.offer(f);
+    }
+    var blah = results.toArray(CompletableFuture[]::new);
+    var retChan = new LinkedBlockingQueue<Object>(1);
+    CompletableFuture.anyOf(blah)
+        .thenAccept(
+            i -> {
+              try {
+                retChan.put(i);
+              } catch (InterruptedException e) {
                 Thread.currentThread().interrupt();
-            }
+              }
+            });
+    return retChan;
+  }
+
+  @SafeVarargs
+  public static CompletableFuture<Object> selectAsync(BlockingQueue<Object>... channels) {
+    var res = select(channels);
+    return CompletableFuture.supplyAsync(
+        () -> {
+          try {
+            return res.take();
+          } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+          }
         });
-        return retChan;
-    }
-
-    public static void goLoop(Runnable f) {
-        Thread.startVirtualThread(() -> {
-            while(true) {
-                f.run();
-            }
-        });
-    }
-
-
-
-    public static BlockingQueue<Object> select(List<BlockingQueue<Object>> channels) {
-        var chans =  channels.toArray(BlockingQueue[]::new);
-        return select(chans);
-    }
-    @SafeVarargs
-    public static  BlockingQueue<Object> select(BlockingQueue<Object> ...channels) {
-        var results = new LinkedBlockingQueue<CompletableFuture<Object>>(channels.length);
-        for (var chan: channels) {
-                var f = CompletableFuture.supplyAsync(() -> {
-                    try {
-                        return chan.take();
-                    } catch (InterruptedException e) {
-                        throw new RuntimeException(e);
-                    }
-                });
-                results.offer(f);
-
-        }
-        var blah = results.toArray(CompletableFuture[]::new);
-        var retChan = new LinkedBlockingQueue<Object>(1);
-        CompletableFuture.anyOf(blah).thenAccept(i -> {
-           try {
-                   retChan.put(i);
-           } catch (InterruptedException e) {
-               Thread.currentThread().interrupt();
-           }
-        });
-        return retChan;
-    }
-
-    @SafeVarargs
-    public static  CompletableFuture<Object> selectAsync(BlockingQueue<Object> ...channels) {
-        var res = select(channels);
-        return CompletableFuture.supplyAsync(() -> {
-            try {
-                return  res.take();
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-        });
-    }
-
+  }
 }
